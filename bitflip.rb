dc751ffbc73185c70c929981d3115f081924a4a4
analca3 <analca3@gmail.com>
2015-11-22 16:26:06 +0100

    Added bitflip

---
+++ bitflip.rb
@@ -0,0 +1,23 @@
+#/usr/bin/ruby
+
+require './rubyeo.rb'
+require 'benchmark'
+
+def timeMutations(number, individual)
+  t = Benchmark.realtime {
+    number.times do |i|
+      mutated = mutate(individual)
+    end
+  }
+  return t
+end
+
+length = 16
+iterations  = 10000
+top = 32768
+
+while length <= top
+  individual = randomChromosome(length)
+  puts "Ruby-BitString, " + length.to_s + ", " + timeMutations(length, individual).to_s
+  length *= 2
+end
